1421bcdf9f050659576489085620e594e9bcb49b
Matilda-bit <polinaovras@gmail.com>
2020-01-19 06:29:42 +0200

    +nav opt,  Header buttton

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -2,12 +2,27 @@ import React from 'react';
 
 import PlaceList from '../components/PlaceList';
 import { PLACE } from '../data/dummy-data';
-import Place from '../models/place';
+
+
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import HeaderButton from '../components/HeaderButton';
 
 
 const FavoritesScreen = props => {
     const favPlaces = PLACE.filter(place => place.id === 'p1' || place.id === 'p2');
    return <PlaceList listData={favPlaces} navigation={props.navigation} />;
+};
+
+FavoritesScreen.navigationOptions = (navData) =>{
+    return {
+        headerTitle: 'Yuor Favorites',
+        headerLeft: 
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item title="Menu" iconName='ios-menu' onPress={() => {
+                    navData.navigation.toggleDrawer();
+                }} />
+            </HeaderButtons>
+    };
 };
 
 FavoritesScreen.navigationOptions ={
