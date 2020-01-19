868423d62d8d10a957f06336691e80d11669f0b5
Matilda-bit <polinaovras@gmail.com>
2020-01-19 02:11:12 +0200

    fix favorite Screen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { 
     View, 
+    Text,
     StyleSheet, 
 } from 'react-native';
 
 
 
 const FavoritesScreen = props => {
-    return (
-        <View>FavoritesScreen!</View>
-        
+   return (
+        <View style = {styles.screen}>
+            <Text>The Favorites Screen!</Text>
+        </View>
     );
 };
 
 const styles= StyleSheet.create ({
-//    screen: {
-//        flex: 1,
-//        padding: 10,
-//        alignItems: 'center'
-//    } 
+   screen: {
+       flex: 1,
+       justifyContent: 'center',
+       alignItems: 'center'
+   } 
 });
 
 export default FavoritesScreen;
