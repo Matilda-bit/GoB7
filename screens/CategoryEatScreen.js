3f8f7c93162868281fbc4a1e2cda4a704b8098f3
Matilda-bit <polinaovras@gmail.com>
2020-01-19 05:39:22 +0200

    clean (use in PlaceList)

--- screens/CategoryEatScreen.js
+++ screens/CategoryEatScreen.js
@@ -1,48 +1,19 @@
 import React from 'react';
-import { View,Text, FlatList, StyleSheet } from 'react-native';
 
 import { CATEGORIES, PLACE } from '../data/dummy-data';
-import MyItem from '../components/MyItem';
+import PlaceList from '../components/PlaceList';
 
 const CategoryEatScreen = props => {
-    //data for my FlatList in renderItem
-    const renderMyItem = itemData => {
-        return (
-            <MyItem 
-                title={itemData.item.title} 
-                location={itemData.item.location} 
-                openingHours={itemData.item.openingHours}
-                image={itemData.item.imageUrl}
-                onSelectedMyItem={() => {
-                    props.navigation.navigate({ 
-                        routeName: 'MyDetail', 
-                        params: {
-                            myId: itemData.item.id
-                        } 
-                    });
-                }}
-
-            />
-        );
-    };
-
+    
     //give a value storing in CategoriesScreen
     const catId = props.navigation.getParam('categoryId');
     //pass data
     //const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
-    const displayedPlaces = PLACE.filter(place => place.categoryId.indexOf(catId) >=0);
+    const displayedPlaces = PLACE.filter(
+        place => place.categoryId.indexOf(catId) >=0);
 
-    return (
-        <View style = {styles.screen}>
-            <FlatList 
-                data={displayedPlaces} 
-                keyExtractor={(item,idex) => item.id } 
-                renderItem={renderMyItem}
-                style={{width: '90%'}}
-                />
-        </View>
-    );
+    return    <PlaceList listData={displayedPlaces} navigation={props.navigation}/>;
 };
 
 //add dinamic configure for headerTitle because we need to use props after fun definition
@@ -57,13 +28,5 @@ CategoryEatScreen.navigationOptions = (navigationData) => {
 };
 
 
-const styles = StyleSheet.create({
-screen:{
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-}
-
-});
 
 export default CategoryEatScreen;
