5017272aa900a4840315607ca831a8d11546596e
Matilda-bit <polinaovras@gmail.com>
2020-01-20 03:09:25 +0200

    import package

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { View,Text, StyleSheet } from 'react-native';
 
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
+
+
 const FiltersScreen = props => {
     return (
         <View style = {styles.screen}>
