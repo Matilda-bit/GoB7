155160b12159589b7a323c34a4112d9744750aed
Matilda-bit <polinaovras@gmail.com>
2020-03-10 16:41:01 +0200

    fix

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { View,Text, StyleSheet } from 'react-native';
+import { 
+    View,
+    Text, 
+    StyleSheet 
+} from 'react-native';
 
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
@@ -8,7 +12,7 @@ import HeaderButton from '../components/HeaderButton';
 const FiltersScreen = props => {
     return (
         <View style = {styles.screen}>
-            <Text>The Filter Screen!</Text>
+            <Text>The Filter Screen!!!</Text>
         </View>
     );
 };
