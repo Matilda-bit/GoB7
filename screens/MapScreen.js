0fb8f1ae0ecccabb0b031a98b3fe7821784ab518
Matilda-bit <polinaovras@gmail.com>
2020-03-10 16:40:43 +0200

    map Screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,18 +1,44 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { 
     View, 
-    StyleSheet, 
+    StyleSheet,
+    Text, 
 } from 'react-native';
 
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
 
 import { MapView, Permissions } from 'expo';
 
 
 const MapScreen = props => {
+ //   const myMap;
     return (
-        <View></View>
+        <View>
+            <Text>Maps</Text>
         
+            <View style={styles}>
+               
+            </View>
+        </View>  
     );
+};
+
+MapScreen.navigationOption = (navData) =>{
+    return {
+        headerTitle: 'Maps',
+        headerLeft: 
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item title="Menu" iconName='ios-menu' onPress={() => {
+                    navData.navigation.toggleDrawer();
+                }} />
+            </HeaderButtons>
+    };
+
+}
+
+MapScreen.navigationOptions ={
+    headerTitle: 'Maps'
 };
 
 const styles= StyleSheet.create ({
