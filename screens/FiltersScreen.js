9280fdf7c76c9b34dfa666356a2eef809e5a94e7
Matilda-bit <polinaovras@gmail.com>
2020-03-17 06:11:26 +0200

    add check box

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -8,11 +8,20 @@ import {
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 import Header from '../components/Header';
+import { Switch } from 'react-native-paper';
 
 const FiltersScreen = props => {
     return (
         <View style = {styles.screen}>
-            <Text>The Filter Screen!!!</Text>
+            <Text style = {styles.title}>Available Filters / Restrictions </Text>
+            <View style = {styles.filterContainer}>
+                <Text>open now</Text>
+                <Switch/>
+            </View>
+            <View style = {styles.filterContainer}>
+                <Text>by closest place</Text>
+                <Switch/>
+            </View>
         </View>
     );
 };
@@ -35,11 +44,22 @@ FiltersScreen.navigationOptions = (navData) =>{
 
 
 const styles = StyleSheet.create({
-screen:{
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-}
+    screen:{
+        flex: 1,
+        alignItems: "center"
+    },
+    title: {
+        fontFamily: 'open-sans-lato',
+        fontSize: 22,
+        margin: 20,
+        textAlign: 'center'
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '80%'
+    }
 
 });
 
