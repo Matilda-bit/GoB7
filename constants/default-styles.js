1803d2ba386de3d13e33ae223385d3e6dec83968
Matilda-bit <polinaovras@gmail.com>
2020-03-17 05:25:02 +0200

    add def styling

--- constants/default-styles.js
+++ constants/default-styles.js
@@ -1,11 +1,17 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    bodyText: {
-        fontFamily: 'open-sans-lato'
-        },
-    title: {
-        fontFamily: 'open-sans-bold',
-        fontSize: 40
+    // bodyText: {
+    //     fontFamily: 'open-sans-lato'
+    //     },
+    // title: {
+    //     fontFamily: 'open-sans-bold',
+    //     fontSize: 40
+    // }
+    screen:  {
+        backgroundColor: '#9c9c9c'
+    },
+    screen2: {
+        backgroundColor: '#2e2e2e'
     }
 });
