05f9e55497f5eddb67fe9346fcede0c544c244b2
Matilda-bit <polinaovras@gmail.com>
2020-03-21 02:51:01 +0200

    passing data between component & nav options

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,27 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { 
     View,
     Text, 
-    StyleSheet 
+    StyleSheet ,
+    Platform
 } from 'react-native';
 
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 import Header from '../components/Header';
 import { Switch } from 'react-native-paper';
+import Colors from '../constants/Colors'
+
+const FilterSwitch = props => {
+    return(
+    <View style = {styles.filterContainer}>
+        <Text>{props.label}</Text>
+        <Switch 
+            trackColor={{true: Colors.accent}}
+            //thumbColor= {Platform.OS === 'android' ? Colors.accent : ''}
+            value={props.state} 
+            onValueChange={props.onChange}
+        />
+    </View>
+    );
+};
 
 const FiltersScreen = props => {
+    const { navigation } = props;
+
+    const [isOpenNow, setIsOpenNow] = useState(false);
+    const [isClosesPlace, setIsClosesPlace] = useState(false);
+
+    //use useEffect to give access to this function whivh is part of my component to my navigation options
+    const saveFilters = useCallback(() => {
+        const appliedFilters = {
+            openNow: isOpenNow,
+            //need to add closes places
+            closesPlace: isClosesPlace
+        };
+
+        console.log(appliedFilters);
+    }, [isOpenNow, isClosesPlace]);
+
+
+    useEffect(() => {
+        navigation.setParams({save: saveFilters});
+    }, [saveFilters]);
+
     return (
         <View style = {styles.screen}>
             <Text style = {styles.title}>Available Filters / Restrictions </Text>
-            <View style = {styles.filterContainer}>
-                <Text>open now</Text>
-                <Switch/>
-            </View>
-            <View style = {styles.filterContainer}>
-                <Text>by closest place</Text>
-                <Switch/>
-            </View>
+            <FilterSwitch 
+                label='open now' 
+                state={isOpenNow} 
+                onChange={newValue => setIsOpenNow(newValue)}
+            />
+            <FilterSwitch 
+                label='by closest place' 
+                state={isClosesPlace} 
+                onChange={newValue => setIsClosesPlace(newValue)}
+            />
+            
         </View>
     );
 };
@@ -32,10 +72,22 @@ FiltersScreen.navigationOptions = (navData) =>{
     return {
         headerTitle: 'Filter Places',
         headerRight: () => <Header />,
+        headerRight: () => 
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                    <Item 
+                        title="Save" 
+                        iconName='ios-save' 
+                        onPress={navData.navigation.getParam('save')} 
+                    />
+            </HeaderButtons>,
+
         //burger menu
         headerLeft:  () =>
             <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                <Item title="Menu" iconName='ios-menu' onPress={() => {
+                <Item 
+                    title="Menu" 
+                    iconName='ios-menu' 
+                    onPress={() => {
                     navData.navigation.toggleDrawer();
                 }} />
             </HeaderButtons>
@@ -58,7 +110,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        width: '80%'
+        width: '80%',
+        marginVertical: 10
     }
 
 });
