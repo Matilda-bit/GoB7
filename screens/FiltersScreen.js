f78b0ac97f932cf71b077bd852c35bc8d586f06e
Matilda-bit <polinaovras@gmail.com>
2020-03-23 21:59:52 +0200

    dispatching filter actions

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -3,14 +3,18 @@ import {
     View,
     Text, 
     StyleSheet ,
+    ScrollView,
     Platform
 } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { Switch } from 'react-native-paper';
+import { useDispatch } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
 import Header from '../components/Header';
 import Colors from '../constants/Colors'
+import { setFilters } from '../store/actions/places';
+// import { ScrollView } from 'react-native-gesture-handler';
 
 const FilterSwitch = props => {
     return(
@@ -27,43 +31,159 @@ const FilterSwitch = props => {
 };
 
 const FiltersScreen = props => {
-    const { navigation } = props;
 
+    const {navigation} = props;
+    //const [isClosesPlace, setIsClosesPlace] = useState(false);
+    const [isOpenShabbath, setIsOpenShabbath] = useState(false);
     const [isOpenNow, setIsOpenNow] = useState(false);
-    const [isClosesPlace, setIsClosesPlace] = useState(false);
+    const [isVegan, setIsVegan] = useState(false);
+    const [isVegetarian, setIsVegetarian] = useState(false);
+    const [isKosher, setIsKosher] = useState(false);
+    const [isNotKosher, setIsNotKosher] = useState(false);
+    const [isCafe, setIsCafe] = useState(false);
+    const [isRestaurant, setIsRestaurant] = useState(false);
+    const [isDelivery, setIsDelivery] = useState(false);
+    const [isTakeAway, setIsTakeAway] = useState(false);
+    const [isFastFood, setIsFastFood] = useState(false);
+    const [isGrill, setIsGrill] = useState(false);
+    const [isSushi, setIsSushi] = useState(false);
+    const [isItalian, setIsItalian] = useState(false);
+    const [isAsian, setIsAsian] = useState(false);
+    const [isEuropean, setIsEuropean] = useState(false);
+    const [isIndian, setIsIndian] = useState(false);
+    
 
+    const dispatch = useDispatch();
     //use useEffect to give access to this function whivh is part of my component to my navigation options
     const saveFilters = useCallback(() => {
         const appliedFilters = {
+            openShabbath: isOpenShabbath,
             openNow: isOpenNow,
+            vegan: isVegan,
+            vegetarian: isVegetarian,
+            kosher: isKosher,
+            notKosher: isNotKosher,
+            cafe: isCafe,
+            restaurant: isRestaurant,
+            delivery: isDelivery,
+            takeAway: isTakeAway,
+            fastFood: isFastFood,
+            grill: isGrill,
+            sushi: isSushi,
+            italian: isItalian,
+            asian: isAsian,
+            european: isEuropean,
+            indian: isIndian
+
             //need to add closes places
-            closesPlace: isClosesPlace
+            //closesPlace: isClosesPlace
+
         };
-
-        console.log(appliedFilters);
-    }, [isOpenNow, isClosesPlace]);
-
+        dispatch(setFilters(appliedFilters));
+        //console.log(appliedFilters);
+    }, [isOpenShabbath, isOpenNow, isVegan, isVegetarian, isKosher, isNotKosher, isCafe, isRestaurant, isDelivery, isTakeAway, isFastFood, isGrill, isSushi, isItalian, isAsian, isEuropean, isIndian, dispatch]);
+    // isOpenNow - must to be the same name from /reducers/places in 'if'
 
     useEffect(() => {
-        navigation.setParams({save: saveFilters});
+        navigation.setParams({ save: saveFilters });
     }, [saveFilters]);
 
-    return (
-        <View style = {styles.screen}>
+        return (
+            <View style = {styles.screen}>
             <Text style = {styles.title}>Available Filters / Restrictions </Text>
+            <FilterSwitch 
+                label='open-Shabbath' 
+                state={isOpenShabbath} 
+                onChange={newValue => setIsOpenShabbath(newValue)}
+            />
             <FilterSwitch 
                 label='open now' 
                 state={isOpenNow} 
                 onChange={newValue => setIsOpenNow(newValue)}
             />
             <FilterSwitch 
+                label='vegan' 
+                state={isVegan} 
+                onChange={newValue => setIsVegan(newValue)}
+            />
+            <FilterSwitch 
+                label='vegetarian' 
+                state={isVegetarian} 
+                onChange={newValue => setIsVegetarian(newValue)}
+            />
+            <FilterSwitch 
+                label='kosher' 
+                state={isKosher} 
+                onChange={newValue => setIsKosher(newValue)}
+            />
+            <FilterSwitch 
+                label='not-Kosher' 
+                state={isNotKosher} 
+                onChange={newValue => setIsNotKosher(newValue)}
+            />
+            <FilterSwitch 
+                label='cafe' 
+                state={isCafe} 
+                onChange={newValue => setIsCafe(newValue)}
+            />
+            <FilterSwitch 
+                label='restaurant' 
+                state={isRestaurant} 
+                onChange={newValue => setIsRestaurant(newValue)}
+            />
+            <FilterSwitch 
+                label='delivery' 
+                state={isDelivery} 
+                onChange={newValue => setIsDelivery(newValue)}
+            />
+            <FilterSwitch 
+                label='takeAway' 
+                state={isTakeAway} 
+                onChange={newValue => setIsTakeAway(newValue)}
+            />
+            <FilterSwitch 
+                label='fastFood' 
+                state={isFastFood} 
+                onChange={newValue => setIsFastFood(newValue)}
+            />
+            <FilterSwitch 
+                label='grill' 
+                state={isGrill} 
+                onChange={newValue => setIsGrill(newValue)}
+            />
+            <FilterSwitch 
+                label='sushi' 
+                state={isSushi} 
+                onChange={newValue => setIsSushi(newValue)}
+            />
+            <FilterSwitch 
+                label='italian' 
+                state={isItalian} 
+                onChange={newValue => setIsItalian(newValue)}
+            />
+            <FilterSwitch 
+                label='asian' 
+                state={isAsian} 
+                onChange={newValue => setIsAsian(newValue)}
+            />
+            <FilterSwitch 
+                label='european' 
+                state={isEuropean} 
+                onChange={newValue => setIsEuropean(newValue)}
+            />
+            <FilterSwitch 
+                label='indian' 
+                state={isIndian} 
+                onChange={newValue => setIsIndian(newValue)}
+            />
+            {/* <FilterSwitch 
                 label='by closest place' 
                 state={isClosesPlace} 
                 onChange={newValue => setIsClosesPlace(newValue)}
-            />
+            /> */}
             
         </View>
-    );
+        );
 };
 
 //134
--- store/reducers/places.js
+++ store/reducers/places.js
@@ -1,5 +1,5 @@
 import { PLACE } from '../../data/dummy-data';
-import { TOGGLE_FAVORITE } from '../actions/places';
+import { TOGGLE_FAVORITE, SET_FILTERS } from '../actions/places';
 
 const initialState = {
     places: PLACE,
@@ -21,6 +21,63 @@ const placesReducer = (state = initialState, action) => {
                     const place = state.places.find(place => place.id === action.placeId);
                     return {...state, favoritePlaces: state.favoritePlaces.concat(place) };
             }
+            case SET_FILTERS:
+                const appliedFilters = action.filters;
+                const updateFilteredPlaces = state.places.filter(place => {
+                    if (appliedFilters.openShabbath && !place.isOpenShabbath){
+                        return false;
+                    }
+                    if (appliedFilters.openNow && !place.isOpenNow){
+                        return false;
+                    }
+                    if (appliedFilters.vegan && !place.isVegan){
+                        return false;
+                    }
+                    if (appliedFilters.vegetarian && !place.isVegetarian){
+                        return false;
+                    }
+                    if (appliedFilters.kosher && !place.isKosher){
+                        return false;
+                    }
+                    if (appliedFilters.notKosher && !place.isNotKosher){
+                        return false;
+                    }
+                    if (appliedFilters.cafe && !place.isCafe){
+                        return false;
+                    }
+                    if (appliedFilters.restaurant && !place.isRestaurant){
+                        return false;
+                    }
+                    if (appliedFilters.delivery && !place.isDelivery){
+                        return false;
+                    }
+                    if (appliedFilters.takeAway && !place.isTakeAway){
+                        return false;
+                    }
+                    if (appliedFilters.fastFood && !place.isFastFood){
+                        return false;
+                    }
+                    if (appliedFilters.grill && !place.isGrill){
+                        return false;
+                    }
+                    if (appliedFilters.sushi && !place.isSushi){
+                        return false;
+                    }
+                    if (appliedFilters.italian && !place.isItalian){
+                        return false;
+                    }
+                    if (appliedFilters.asian && !place.isAsian){
+                        return false;
+                    }
+                    if (appliedFilters.european && !place.isEuropean){
+                        return false;
+                    }
+                    if (appliedFilters.indian && !place.isIndian){
+                        return false;
+                    }
+                    return true;
+                });
+                return {...state, filteredPlaces: updateFilteredPlaces};
         default: 
             return state;
     }
