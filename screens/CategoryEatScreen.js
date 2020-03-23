c7bf5dfdbadb7d742541d80f1aaaf6b9ee86bfb8
Matilda-bit <polinaovras@gmail.com>
2020-03-23 22:59:21 +0200

    display no data found

--- screens/CategoryEatScreen.js
+++ screens/CategoryEatScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 
 
 import { CATEGORIES } from '../data/dummy-data';
 import PlaceList from '../components/PlaceList';
 import Header from '../components/Header';
+import BodyText from '../components/BodyText';
 
 const CategoryEatScreen = props => {
     
@@ -17,7 +19,12 @@ const CategoryEatScreen = props => {
 
     const displayedPlaces = availablePlaces.filter(
         place => place.categoryId.indexOf(catId) >=0);
+    if (displayedPlaces.length === 0 || displayedPlaces === null){
+        return <View style={styles.content}>
+            <BodyText>No data found, maybe check your filters?</BodyText>
+            </View>
 
+    };
     return    <PlaceList 
                 listData={displayedPlaces} 
                 navigation={props.navigation}
@@ -30,7 +37,7 @@ CategoryEatScreen.navigationOptions = navigationData => {
     const catId= navigationData.navigation.getParam('categoryId');
     
     const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
-
+    
     return {
         headerTitle: selectedCategory.title ,
         headerRight: () => <Header />
@@ -44,6 +51,13 @@ CategoryEatScreen.navigationOptions = navigationData => {
     };
 };
 
-
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+    }
+});
 
 export default CategoryEatScreen;
