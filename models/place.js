6ca6bdf5b7e0ffec8e1eafa908d0f65f765c3c8a
Matilda-bit <polinaovras@gmail.com>
2020-03-23 21:57:38 +0200

    adding fields

--- models/place.js
+++ models/place.js
@@ -5,7 +5,24 @@ class Place {
         title,
         imageUrl,
         location,
-        openingHours     
+        openingHours,
+        isOpenShabbath,
+        isOpenNow,
+        isVegan,
+        isVegetarian,
+        isKosher,
+        isNotKosher,
+        isCafe,
+        isRestaurant,
+        isDelivery,
+        isTakeAway,
+        isFastFood,
+        isGrill,
+        isSushi,
+        isItalian,
+        isAsian,
+        isEuropean,
+        isIndian
         ) {
             this.id = id;
             this.categoryId = categoryId;
@@ -13,6 +30,23 @@ class Place {
             this.imageUrl = imageUrl;
             this.location = location;
             this.openingHours = openingHours;
+            this.isOpenShabbath = isOpenShabbath;
+            this.isOpenNow = isOpenNow;
+            this.isVegan = isVegan;
+            this.isVegetarian = isVegetarian;
+            this.isKosher = isKosher;
+            this.isNotKosher = isNotKosher;
+            this.isCafe = isCafe;
+            this.isRestaurant = isRestaurant;
+            this.isDelivery = isDelivery;
+            this.isTakeAway = isTakeAway;
+            this.isFastFood = isFastFood;
+            this.isGrill = isGrill;
+            this.isSushi = isSushi;
+            this.isItalian = isItalian;
+            this.isAsian = isAsian;
+            this.isEuropean = isEuropean;
+            this.isIndian = isIndian;
         }
 }
 
