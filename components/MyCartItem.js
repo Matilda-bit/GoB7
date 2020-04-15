091abbfc0f621a588e982614fdb0742365b74b38
Matilda-bit <polinaovras@gmail.com>
2020-04-15 23:09:42 +0300

    not used

--- components/MyCartItem.js
+++ components/MyCartItem.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import { View, Text, StyleSheet, TouchableOpacity, Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+//don't need this anymore
 const MyCartItem = props => {
-    return (<View></View>);
+    return (
+        <View>
+            <Text>
+                <Text> QTY </Text>
+                <Text> TITLE </Text>
+            </Text>
+            <View>
+                <Text>SAMT</Text>
+
+                <TouchableOpacity>
+                    <Ionicons name={Platform.OS === 'android' ? 'md-trash': 'ios-trash'}
+                    size={23}
+                    color="red"
+                    />
+                </TouchableOpacity>
+            </View>
+
+
+        </View>
+    );
 };
 const styles = StyleSheet.create({});
 
