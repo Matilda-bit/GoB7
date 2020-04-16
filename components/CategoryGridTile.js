8772a8dbbbf23b4815b20a56fa86236d97d2f847
Matilda-bit <polinaovras@gmail.com>
2020-04-16 16:49:21 +0300

    display image in my category grid

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, StyleSheet, Platform, TouchableNativeFeedback } from 'react-native';
+import { TouchableOpacity, View, Text, ImageBackground, StyleSheet, Platform, TouchableNativeFeedback, ImageBackgroundBase } from 'react-native';
 
 const CategoryGridTile = props => {
     let TouchableCmp = TouchableOpacity;
@@ -10,10 +10,13 @@ const CategoryGridTile = props => {
     return(
         <View style={styles.gridItem}>
             <TouchableCmp style={{flex: 1}} onPress = {props.onSelect}>
-                <View 
-                style={{ ...styles.container, ...{ backgroundColor: props.color} }}
-                >
+                <View>
+                    <ImageBackground 
+                    source={{uri: props.image}}
+                    style={styles.bgImage}
+                    >
                     <Text style={styles.title} numberOfLines ={2} >{props.title} </Text>
+                    </ImageBackground>
                 </View>
             </TouchableCmp>
         </View>
@@ -24,33 +27,46 @@ const CategoryGridTile = props => {
 const styles = StyleSheet.create({
     gridItem: {
         flex: 1,
-        margin: 15,
-        height: 150,
-        borderRadius: 10,
+        margin: 10,
+        height: 165,
+        borderRadius: 25,
+        borderColor:'black',
+        borderWidth: 1,
         overflow: Platform.OS === 'android' && Platform.Version >= 21 
         ? 'hidden' 
         : 'visible',
-        elevation: 5
+        elevation: 5,
+        
     },
     container: {
         flex: 1,
-        borderRadius: 10,
         shadowColor: 'black',
-        shadowOpacity: 0.26,
+        shadowOpacity: 0.32,
         shadowOffset: {width: 0, height: 2},
-        shadowRadius: 10,
+        shadowRadius: 25,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
         
-        padding: 20,
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end'
     },
     title: {
         fontFamily: 'open-sans-lato',
-        fontSize: 25,
-        textAlign: 'left',
-        color: 'white'
+        fontSize: 23,
+        textAlign: 'center',
+        color: 'white',
+        //textDecorationLine: 'underline',
+        textShadowOffset: {width: 0, height: 2},
+        textShadowColor: 'black',
+        textShadowRadius: 1,
+        backgroundColor: 'rgba(0,0,0,0.5)'
 
+    },
+    bgImage: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        //backgroundColor: 'rgba(0,0,0,0.5)'
     }
+
 });
 
 export default CategoryGridTile;
