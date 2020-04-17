68294b31f148ce059010c8f507f9d13bfdb3f6f1
Matilda-bit <polinaovras@gmail.com>
2020-04-17 03:57:34 +0300

    add handler func, use Selector

--- screens/user/UserPlacesScreen.js
+++ screens/user/UserPlacesScreen.js
@@ -1,28 +1,78 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import PlaceList from '../../components/PlaceList';
-import { useSelector } from 'react-redux';
-
+import { View, StyleSheet, FlatList, Button, Platform, Alert } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
+import ProductItem from '../../components/ProductItem';
 import BodyText from '../../components/text/BodyText';
 import Header from '../../components/Header';
+import Colors from '../../constants/Colors';
+import * as placesActions from '../../store/actions/places';
+//import { FlatList } from 'react-native-gesture-handler';
 //import Colors from '../../constants/Colors';
 
 
 const UserPlacesScreen = props => {
           
     //pass data from store/reducer/places
-    const displayedPlaces = useSelector(state => state.places.userPlaces);
+    const userPlaces = useSelector(state => state.places.userPlaces);
+    const dispatch = useDispatch();
 
-    if (displayedPlaces.length === 0 || displayedPlaces === null){
-        return <View style={styles.content}>
-            <BodyText>No data found, maybe you want to add your place?</BodyText>
-            </View>
 
+    if (userPlaces.length === 0 || userPlaces === null){
+      return <View style={styles.content}>
+          <BodyText>No data found, maybe you want to add your place?</BodyText>
+          </View>
     };
-  return (<PlaceList listData={displayedPlaces} navigation={props.navigation}/>);
+   
+    
+    const editPlaceHandler = id => {
+      props.navigation.navigate('EditPlace', { placeId: id});
+    };
+
+    const deleteHandler = (id) => {
+      Alert.alert('Are you sure?','Do you really want to delete this place?', [
+        { text: 'No', style: 'default' },
+        {
+          text: 'Yes',
+          style: 'destructive',
+          onPress: () => {
+            dispatch(placesActions.deletePlace(id));
+          }
+        }
+      ]);
+    };
+    
+    return (
+      <FlatList
+          data={userPlaces}
+          keyExtractor={item => item.id}
+          renderItem={itemData => (
+            <ProductItem
+              image={itemData.item.imageUrl}
+              title={itemData.item.title}
+              onSelect={()=> {
+                editPlaceHandler(itemData.item.id);
+              }}
+              >
+                <Button
+                color={Colors.primary}
+                title="Edit"
+                onPress={() => {
+                  editPlaceHandler(itemData.item.id)
+                }}
+                />
+                <Button
+                color={Colors.accentColor}
+                title="Delete"
+                onPress={() => {
+                  deleteHandler.bind(this, itemData.item.id);
+                }}
+                />
+            </ProductItem>
+        )} 
+        />);
 };
 
 
@@ -30,6 +80,17 @@ UserPlacesScreen.navigationOptions = navData => {
     return {
       headerTitle: 'Your Places',
       headerRight: () => <Header />,
+      // headerRight: () => (
+      //   <HeaderButtons HeaderButtonComponent={HeaderButton}>
+      //     <Item
+      //       title="Add"
+      //       iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+      //       onPress={() => {
+      //         navData.navigation.navigate('EditPlace');
+      //       }}
+      //     />
+      //   </HeaderButtons>
+      // ),
       headerLeft: () => (
         <HeaderButtons HeaderButtonComponent={HeaderButton}>
           <Item
@@ -44,10 +105,10 @@ UserPlacesScreen.navigationOptions = navData => {
 };
 
 const styles = StyleSheet.create({
-    content: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+  content: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+  }
 });
 export default UserPlacesScreen;
