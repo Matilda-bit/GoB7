34395198d5f9b6fc3951e6050ba01e0799e6fac9
Matilda-bit <polinaovras@gmail.com>
2020-04-17 03:49:56 +0300

    object simplification

--- models/place.js
+++ models/place.js
@@ -1,3 +1,54 @@
+// class Place {
+//     constructor(
+//         id,
+//         categoryId,
+//         title,
+//         imageUrl,
+//         location,
+//         openingHours,
+//         isOpenShabbath,
+//         isOpenNow,
+//         isVegan,
+//         isVegetarian,
+//         isKosher,
+//         isNotKosher,
+//         isCafe,
+//         isRestaurant,
+//         isDelivery,
+//         isTakeAway,
+//         isFastFood,
+//         isGrill,
+//         isSushi,
+//         isItalian,
+//         isAsian,
+//         isEuropean,
+//         isIndian
+//         ) {
+//             this.id = id;
+//             this.categoryId = categoryId;
+//             this.title = title;
+//             this.imageUrl = imageUrl;
+//             this.location = location;
+//             this.openingHours = openingHours;
+//             this.isOpenShabbath = isOpenShabbath;
+//             this.isOpenNow = isOpenNow;
+//             this.isVegan = isVegan;
+//             this.isVegetarian = isVegetarian;
+//             this.isKosher = isKosher;
+//             this.isNotKosher = isNotKosher;
+//             this.isCafe = isCafe;
+//             this.isRestaurant = isRestaurant;
+//             this.isDelivery = isDelivery;
+//             this.isTakeAway = isTakeAway;
+//             this.isFastFood = isFastFood;
+//             this.isGrill = isGrill;
+//             this.isSushi = isSushi;
+//             this.isItalian = isItalian;
+//             this.isAsian = isAsian;
+//             this.isEuropean = isEuropean;
+//             this.isIndian = isIndian;
+//         }
+// }
 class Place {
     constructor(
         id,
@@ -7,22 +58,7 @@ class Place {
         location,
         openingHours,
         isOpenShabbath,
-        isOpenNow,
-        isVegan,
-        isVegetarian,
-        isKosher,
-        isNotKosher,
-        isCafe,
-        isRestaurant,
-        isDelivery,
-        isTakeAway,
-        isFastFood,
-        isGrill,
-        isSushi,
-        isItalian,
-        isAsian,
-        isEuropean,
-        isIndian
+        isOpenNow
         ) {
             this.id = id;
             this.categoryId = categoryId;
@@ -32,22 +68,6 @@ class Place {
             this.openingHours = openingHours;
             this.isOpenShabbath = isOpenShabbath;
             this.isOpenNow = isOpenNow;
-            this.isVegan = isVegan;
-            this.isVegetarian = isVegetarian;
-            this.isKosher = isKosher;
-            this.isNotKosher = isNotKosher;
-            this.isCafe = isCafe;
-            this.isRestaurant = isRestaurant;
-            this.isDelivery = isDelivery;
-            this.isTakeAway = isTakeAway;
-            this.isFastFood = isFastFood;
-            this.isGrill = isGrill;
-            this.isSushi = isSushi;
-            this.isItalian = isItalian;
-            this.isAsian = isAsian;
-            this.isEuropean = isEuropean;
-            this.isIndian = isIndian;
         }
 }
-
 export default Place;
