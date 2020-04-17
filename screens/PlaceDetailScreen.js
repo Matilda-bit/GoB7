034d0b67a16b096b44680f8ad66e7a6ef2d6d120
Matilda-bit <polinaovras@gmail.com>
2020-04-17 19:22:37 +0300

    fix a problem

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -44,8 +44,9 @@ const PlaceDetailScreen = props => {
             </View>
       
             <View style={styles.myDetail}>          
-                <WhiteBodyText>category:  {selectedItem.categoryId.map(categoryId => <WhiteBodyText key={categoryId}>{categoryId} </WhiteBodyText>)}</WhiteBodyText>
-               
+                {/* <WhiteBodyText>category:  {selectedItem.categoryId.map(categoryId => <WhiteBodyText key={categoryId}>{categoryId} </WhiteBodyText>)}</WhiteBodyText>
+                */}
+                <WhiteBodyText>category:  {selectedItem.categoryId}</WhiteBodyText>
                 <WhiteBodyText>address: {selectedItem.location}, Be'er sheva</WhiteBodyText>
                 <WhiteBodyText>opening hours: {selectedItem.openingHours.toUpperCase()}</WhiteBodyText> 
             </View> 
