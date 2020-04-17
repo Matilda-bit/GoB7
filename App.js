30a3134507099c3c6a169bf699ecf660232ab88d
Matilda-bit <polinaovras@gmail.com>
2020-04-17 03:47:34 +0300

    use applyMiddleware

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { useState }  from 'react';
 import { StyleSheet, View } from 'react-native';
 import * as Font from 'expo-font';
+import ReduxThunk from 'redux-thunk';
+
 import { AppLoading } from 'expo';
 import {  enableScreens } from 'react-native-screens';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import PlacesNavigator from './navigation/PlacesNavigator';
@@ -15,7 +17,7 @@ const rootReducer = combineReducers({
   places: placesReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 const fetchFonts = () => {
   return Font.loadAsync({
