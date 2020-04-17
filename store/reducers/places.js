330c5ecbf918331265864f5adfae5e9968f2f0c8
Matilda-bit <polinaovras@gmail.com>
2020-04-17 03:59:05 +0300

    minimaze filters,add import place,fix CREATE_PLACE

--- store/reducers/places.js
+++ store/reducers/places.js
@@ -1,4 +1,4 @@
-import { PLACE, CATEGORIES } from '../../data/dummy-data';
+import { PLACE } from '../../data/dummy-data';
 import { 
     TOGGLE_FAVORITE, 
     SET_FILTERS, 
@@ -6,9 +6,9 @@ import {
     CREATE_PLACE,
     UPDATE_PLACE 
 } from '../actions/places';
-
+import Place from '../../models/place';
 const initialState = {
-    category: CATEGORIES,
+    //category: CATEGORIES,
     places: PLACE,
     filteredPlaces: PLACE,
     favoritePlaces: [],
@@ -39,65 +39,88 @@ const placesReducer = (state = initialState, action) => {
                 if (appliedFilters.openNow && !place.isOpenNow){
                     return false;
                 }
-                if (appliedFilters.vegan && !place.isVegan){
-                    return false;
-                }
-                if (appliedFilters.vegetarian && !place.isVegetarian){
-                    return false;
-                }
-                if (appliedFilters.kosher && !place.isKosher){
-                    return false;
-                }
-                if (appliedFilters.notKosher && !place.isNotKosher){
-                    return false;
-                }
-                if (appliedFilters.cafe && !place.isCafe){
-                    return false;
-                }
-                if (appliedFilters.restaurant && !place.isRestaurant){
-                    return false;
-                }
-                if (appliedFilters.delivery && !place.isDelivery){
-                    return false;
-                }
-                if (appliedFilters.takeAway && !place.isTakeAway){
-                    return false;
-                }
-                if (appliedFilters.fastFood && !place.isFastFood){
-                    return false;
-                }
-                if (appliedFilters.grill && !place.isGrill){
-                    return false;
-                }
-                if (appliedFilters.sushi && !place.isSushi){
-                    return false;
-                }
-                if (appliedFilters.italian && !place.isItalian){
-                    return false;
-                }
-                if (appliedFilters.asian && !place.isAsian){
-                    return false;
-                }
-                if (appliedFilters.european && !place.isEuropean){
-                    return false;
-                }
-                if (appliedFilters.indian && !place.isIndian){
-                    return false;
-                }
+                // if (appliedFilters.vegan && !place.isVegan){
+                //     return false;
+                // }
+                // if (appliedFilters.vegetarian && !place.isVegetarian){
+                //     return false;
+                // }
+                // if (appliedFilters.kosher && !place.isKosher){
+                //     return false;
+                // }
+                // if (appliedFilters.notKosher && !place.isNotKosher){
+                //     return false;
+                // }
+                // if (appliedFilters.cafe && !place.isCafe){
+                //     return false;
+                // }
+                // if (appliedFilters.restaurant && !place.isRestaurant){
+                //     return false;
+                // }
+                // if (appliedFilters.delivery && !place.isDelivery){
+                //     return false;
+                // }
+                // if (appliedFilters.takeAway && !place.isTakeAway){
+                //     return false;
+                // }
+                // if (appliedFilters.fastFood && !place.isFastFood){
+                //     return false;
+                // }
+                // if (appliedFilters.grill && !place.isGrill){
+                //     return false;
+                // }
+                // if (appliedFilters.sushi && !place.isSushi){
+                //     return false;
+                // }
+                // if (appliedFilters.italian && !place.isItalian){
+                //     return false;
+                // }
+                // if (appliedFilters.asian && !place.isAsian){
+                //     return false;
+                // }
+                // if (appliedFilters.european && !place.isEuropean){
+                //     return false;
+                // }
+                // if (appliedFilters.indian && !place.isIndian){
+                //     return false;
+                // }
                 return true;
             });
             
         return {...state, filteredPlaces: updateFilteredPlaces};
+        // case CREATE_PLACE:
+        //     const newPlace = new Place(
+        //         new Date().toString(),
+        //         'u1',
+        //         action.placeData.title,
+        //         action.placeData.imageUrl
+        //     );
+        //     return {
+        //         ...state,
+        //         availablePlaces: state.availablePlaces.concat(newPlace),
+        //         userPlaces: state.userPlaces.concat(newPlace)
+        //     };
         case CREATE_PLACE:
+            // console.log('TEST');
+            // console.log(action.placeData.categoryId);
+            // console.log(action.placeData.title);
+            // console.log(action.placeData.imageUrl);
+            // console.log(action.placeData.location);
+            // console.log(action.placeData.openingHours);
             const newPlace = new Place(
-                new Date().toString(),
-                'u1',
+                action.placeData.id,
+                action.placeData.categoryId,
                 action.placeData.title,
-                action.placeData.imageUrl
+                action.placeData.imageUrl,
+                action.placeData.location,
+                action.placeData.openingHours,
+                false,
+                false
             );
             return {
                 ...state,
-                availablePlaces: state.availablePlaces.concat(newPlace),
+                places: state.places.concat(newPlace),
+                filteredPlaces: state.places.concat(newPlace),
                 userPlaces: state.userPlaces.concat(newPlace)
             };
         case UPDATE_PLACE:
@@ -105,21 +128,23 @@ const placesReducer = (state = initialState, action) => {
                 prod => prod.id === action.placeId
             );
             const updatedPlace = new Place(
-                action.placeId,
+                //action.placeId,
                 state.userPlaces[placeIndex].ownerId,
                 action.placeData.title,
-                action.placeData.imageUrl
+                action.placeData.imageUrl,
+                action.placeData.location,
+                action.placeData.openingHours,
             );
             const updatedUserPlaces = [...state.userPlaces];
             updatedUserPlaces[placeIndex] = updatedPlace;
-            const availablePlaceIndex = state.availablePlaces.findIndex(
+            const availablePlaceIndex = state.places.findIndex(
                 prod => prod.id === action.placeId
             );
-            const updatedAvailablePlaces = [...state.availablePlaces];
+            const updatedAvailablePlaces = [...state.places];
             updatedAvailablePlaces[availablePlaceIndex] = updatedPlace;
             return {
                 ...state,
-                availablePlaces: updatedAvailablePlaces,
+                places: updatedAvailablePlaces,
                 userPlaces: updatedUserPlaces
             };
         case DELETE_PLACE:
@@ -128,7 +153,7 @@ const placesReducer = (state = initialState, action) => {
                 userPlaces: state.userPlaces.filter(
                 place => place.id !== action.placeId
                 ),
-                availablePlaces: state.availablePlaces.filter(
+                places: state.places.filter(
                 place => place.id !== action.placeId
                 )
             };
