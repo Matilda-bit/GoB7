9bfd0290afe43fcaf87804ac504d6e63f585bf2d
Matilda-bit <polinaovras@gmail.com>
2020-04-20 00:09:58 +0300

    start write a fun for fetch DB

--- store/actions/places.js
+++ store/actions/places.js
@@ -1,8 +1,44 @@
+import Place from '../../models/place';
+
 export const TOGGLE_FAVORITE = 'TOGGLE_FAVORITE';
 export const SET_FILTERS = 'SET_FILTERS';
+//admin zone
 export const DELETE_PLACE = 'DELETE_PLACE';
 export const CREATE_PLACE = 'CREATE_PLACE';
 export const UPDATE_PLACE = 'UPDATE_PLACE';
+//fetch db
+//export const SET_PLACES = 'SET_PLACES';
+
+// export const fetchPlaces = () => {
+//   return async dispatch => {
+//     // any async code you want!
+//     //method: 'GET',
+//     //don't need 'header' and 'body' anymore
+//     const response = await fetch('https://my-project-fdbf6.firebaseio.com/places.json');
+
+//     //unpacked data
+//     const resData = await response.json();
+//     //console.log(resData);
+//     const loadedPlaces = [];
+
+//     for (const key in resData){
+//       loadedPlaces.push(
+//         new Place(
+//           key,
+//           [resData[key].categoryId],
+//           resData[key].title,
+//           'u1',
+//           resData[key].imageUrl,
+//           resData[key].location,
+//           resData[key].openingHours,
+//           false,
+//           false,
+//         )
+//       );
+//     }
+//     dispatch({ type: SET_PLACES, places: loadedPlaces });
+//   };
+// }; 
 
 export const deletePlace = id => {
     return { type: DELETE_PLACE, placeId: id };
@@ -30,20 +66,20 @@ export const deletePlace = id => {
       //console.log(resData);
       
       dispatch({
-      type: CREATE_PLACE,
-      placeData: {
-        id: resData.name,
-        categoryId,
-        title,
-        imageUrl,
-        location,
-        openingHours
-        }  
+        type: CREATE_PLACE,
+        placeData: {
+          id: resData.name,
+          categoryId,
+          title,
+          imageUrl,
+          location,
+          openingHours
+          }  
       });  
     };  
   };
 
-export const updatePlace = (id,categoryId, title, imageUrl, location, openingHours) => {
+export const updatePlace = (id, categoryId, title, imageUrl, location, openingHours) => {
     return {
         type: UPDATE_PLACE,
         placeId: id,
