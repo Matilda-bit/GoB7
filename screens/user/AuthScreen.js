1f4bc3e7fb5e3eccf7e0786a460666e250afa99b
Matilda-bit <polinaovras@gmail.com>
2020-04-24 16:38:59 +0300

    basic login screen

---
+++ screens/user/AuthScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet, ScrollView, KeyboardAvoidingView } from 'react-native';
+
+import Input from '../../components/UI/Input';
+
+const AuthScreen = props => {
+    return (
+        <View style={styles.screen}></View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {}
+});
+
+export default AuthScreen;
