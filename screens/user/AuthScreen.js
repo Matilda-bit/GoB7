7604869facb39f44c5eeef531efae02687d80129
Matilda-bit <polinaovras@gmail.com>
2020-05-24 17:56:15 +0300

    basic login screen

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,16 +1,85 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView, KeyboardAvoidingView } from 'react-native';
+import { View, StyleSheet, ScrollView, KeyboardAvoidingView,  Button } from 'react-native';
 
 import Input from '../../components/UI/Input';
+import Card from '../../components/UI/Card';
+import Colors from '../../constants/Colors';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const AuthScreen = props => {
     return (
-        <View style={styles.screen}></View>
+        <KeyboardAvoidingView 
+            behavior="padding"
+            keyboardVerticalOffset={50}
+            style={styles.screen}
+        >
+        <LinearGradient colors={['#ffedff', '#ffe3ff']} style={styles.gradient}>
+            <Card style={styles.authContainer} >
+                <ScrollView>
+                <Input
+                id="email"
+                label="E-Mail"
+                keyboardType="email-address"
+                required
+                email
+                autoCapitalize="none"
+                errorMessage="Please enter a valid email address."
+                onInputChange={() => {}}
+                initialValue=""
+              />
+              <Input
+                id="password"
+                label="Password"
+                keyboardType="default"
+                secureTextEntry
+                required
+                minLength={5}
+                autoCapitalize="none"
+                errorMessage="Please enter a valid password."
+                onInputChange={() => {}}
+                initialValue=""
+              />
+              <View style={styles.buttonContainer}>
+                <Button title="Login" color={Colors.primary} onPress={() => {}} />
+              </View>
+              <View style={styles.buttonContainer}>
+                <Button
+                  title="Switch to Sign Up"
+                  color={Colors.accent}
+                  onPress={() => {}}
+                />
+              </View>
+            
+                </ScrollView>
+            </Card>
+            </LinearGradient>
+        </KeyboardAvoidingView>
+        
+        //<View style={styles.screen}></View>
     );
 };
+AuthScreen.navigationOptions = {
+    headerTitle: 'Authenticate'
+  };
 
 const styles = StyleSheet.create({
-    screen: {}
+    screen: {
+        flex: 1
+    },
+    gradient: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+    authContainer: {
+        width: '80%',
+        maxWidth: 400,
+        maxHeight: 400,
+        padding: 20
+    },
+    buttonContainer: {
+        marginTop: 10
+      }
 });
 
 export default AuthScreen;
