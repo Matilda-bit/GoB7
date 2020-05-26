9ef6b8ab2ca7f822f2f3a20fd6976d555b01c286
Matilda-bit <polinaovras@gmail.com>
2020-05-27 01:12:45 +0300

    Showing a Map Preview of the Location

--- components/UI/LocationPicker.js
+++ components/UI/LocationPicker.js
@@ -11,6 +11,8 @@ import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
 import Colors from '../../constants/Colors';
+import MapPreview from './MapPreview';
+
 
 const LocationPicker = props => {
   const [isFetching, setIsFetching] = useState(false);
@@ -40,15 +42,14 @@ const LocationPicker = props => {
       const location = await Location.getCurrentPositionAsync({
         timeout: 5000
       });
-      console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
-      console.log(location);
-      setPickedLocation(null);
-    //   setPickedLocation({
-    //     lat: location.coords.latitude,
-    //     lng: location.coords.longitude
-    //   });
-    console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
-    console.log(location);
+     // console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+      //console.log(location);
+      //setPickedLocation(null);
+      setPickedLocation({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude
+      });
+    //console.log(location);
     } catch (err) {
       Alert.alert(
         'Could not fetch location!',
@@ -61,13 +62,14 @@ const LocationPicker = props => {
 
   return (
     <View style={styles.locationPicker}>
-      <View style={styles.mapPreview}>
+      <MapPreview style={styles.mapPreview} location={pickedLocation}>
         {isFetching ? (
           <ActivityIndicator size="large" color={Colors.primary} />
         ) : (
           <Text>No location chosen yet!</Text>
         )}
-      </View>
+      </MapPreview>
+        
       <Button
         title="Get User Location"
         color={Colors.primary}
@@ -87,8 +89,7 @@ const styles = StyleSheet.create({
     height: 150,
     borderColor: '#ccc',
     borderWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+
   }
 });
 
---
+++ env.js
@@ -0,0 +1,5 @@
+const vars = {
+    googleApiKey: 'AIzaSyAcyE9aR7u_0503lIE-ww-Si3clqxHzCj8'
+};
+
+export default vars;
