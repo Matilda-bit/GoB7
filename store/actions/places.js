517251188981caaaeb0881427ee346550e875cbb
Matilda-bit <polinaovras@gmail.com>
2020-05-28 20:38:04 +0300

    remove from here  filters & favPlaces

--- store/actions/places.js
+++ store/actions/places.js
@@ -1,203 +1,223 @@
-import Place from '../../models/place';
+import Place from "../../models/place";
 //my actions
-export const TOGGLE_FAVORITE = 'TOGGLE_FAVORITE';
-export const SET_FILTERS = 'SET_FILTERS';
+// export const TOGGLE_FAVORITE = 'TOGGLE_FAVORITE';
+// export const SET_FILTERS = 'SET_FILTERS';
 //admin zone
-export const DELETE_PLACE = 'DELETE_PLACE';
-export const CREATE_PLACE = 'CREATE_PLACE';
-export const UPDATE_PLACE = 'UPDATE_PLACE';
+export const DELETE_PLACE = "DELETE_PLACE";
+export const CREATE_PLACE = "CREATE_PLACE";
+export const UPDATE_PLACE = "UPDATE_PLACE";
 //fetch db
-export const SET_PLACES = 'SET_PLACES';
+export const SET_PLACES = "SET_PLACES";
 
 export const fetchPlaces = () => {
-  return async dispatch => {
+  return async (dispatch) => {
     // any async code you want!
     //method: 'GET',
     //don't need 'header' and 'body' anymore
     try {
       const response = await fetch(
-        'https://my-project-fdbf6.firebaseio.com/places.json'
-        );
+        "https://my-project-fdbf6.firebaseio.com/places.json"
+      );
 
       //
-      if ( !response.ok ) {
-          throw new Error('Polina something went wrong!');
+      if (!response.ok) {
+        throw new Error("Polina something went wrong!");
       }
       //unpacked data
       const resData = await response.json();
       //console.log(resData);
       const loadedPlaces = [];
-  
-      for (const key in resData){
+
+      for (const key in resData) {
         loadedPlaces.push(
           new Place(
             key,
             resData[key].categoryId,
-            'u1',
+            "u1",
             resData[key].title,
             resData[key].imageUrl,
             resData[key].location,
             resData[key].openingHours,
             false,
-            false,
+            false
           )
         );
       }
       //console.log(resData);
       dispatch({ type: SET_PLACES, places: loadedPlaces });
     } catch (err) {
-        // can to add - send to custom analytics server
-        throw err;
+      // can to add - send to custom analytics server
+      throw err;
     }
-    
   };
-}; 
+};
 
-export const deletePlace = id => {
-  
+export const deletePlace = (id) => {
   return async (dispatch, getState) => {
     const token = getState().auth.token;
     console.log(getState());
-    const response = await fetch (
-      `https://my-project-fdbf6.firebaseio.com/places/${id}.json?auth=${token}`, {
-        method: 'DELETE'
+    const response = await fetch(
+      `https://my-project-fdbf6.firebaseio.com/places/${id}.json?auth=${token}`,
+      {
+        method: "DELETE",
       }
     );
 
-      if (!response.ok) {
-        throw new Error('Something went wrong! catch deletePlace');
-      }
-      dispatch({ type: DELETE_PLACE, id: id });
-      };
+    if (!response.ok) {
+      throw new Error("Something went wrong! catch deletePlace");
+    }
+    dispatch({ type: DELETE_PLACE, id: id });
   };
-  
+};
 
-  export const createPlace = (categoryId, title, imageUrl, location, openingHours) => {
-    return async (dispatch, getState)  => {
-      // any async code you want!
-      const token = getState().auth.token;
-      console.log(getState());
-      const response = await fetch(
-        `https://my-project-fdbf6.firebaseio.com/places.json?auth=${token}`, {
-        method: 'POST',
+export const createPlace = (
+  categoryId,
+  title,
+  imageUrl,
+  location,
+  openingHours
+) => {
+  return async (dispatch, getState) => {
+    // any async code you want!
+    const token = getState().auth.token;
+    console.log(getState());
+    const response = await fetch(
+      `https://my-project-fdbf6.firebaseio.com/places.json?auth=${token}`,
+      {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           categoryId,
           title,
           imageUrl,
           location,
-          openingHours
-        })
-      });
-      
-      //unpacked data
-      const resData = await response.json();
-      //console.log(resData);
-      if (!response.ok) {
-        throw new Error('Something went wrong! catch createPlace');
+          openingHours,
+        }),
       }
-      
-      dispatch({
-        type: CREATE_PLACE,
-        placeData: {
-          id: resData.name,
-          categoryId,
-          title,
-          imageUrl,
-          location,
-          openingHours
-          }  
-      });  
-    };  
-  };
+    );
 
-export const updatePlace = (id, categoryId, title, imageUrl, location, openingHours) => {
-  console.log('======================================================================================');
-  console.log('======================================================================================');  
-  console.log('======================================================================================');  
-  console.log('======================================================================================');
-    console.log(id);
-    console.log(title);
+    //unpacked data
+    const resData = await response.json();
+    //console.log(resData);
+    if (!response.ok) {
+      throw new Error("Something went wrong! catch createPlace");
+    }
+
+    dispatch({
+      type: CREATE_PLACE,
+      placeData: {
+        id: resData.name,
+        categoryId,
+        title,
+        imageUrl,
+        location,
+        openingHours,
+      },
+    });
+  };
+};
+
+export const updatePlace = (
+  id,
+  categoryId,
+  title,
+  imageUrl,
+  location,
+  openingHours
+) => {
+  console.log(
+    "======================================================================================"
+  );
+  console.log(
+    "======================================================================================"
+  );
+  console.log(
+    "======================================================================================"
+  );
+  console.log(
+    "======================================================================================"
+  );
+  console.log(id);
+  console.log(title);
   return async (dispatch, getState) => {
     //console.log (getState());
     const token = getState().auth.token;
     console.log(getState());
     const response = await fetch(
-      `https://my-project-fdbf6.firebaseio.com/places/${id}.json?auth=${token}`, 
-        {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json'
-          },
+      `https://my-project-fdbf6.firebaseio.com/places/${id}.json?auth=${token}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({
           categoryId,
           title,
           imageUrl,
           location,
-          openingHours
-        })
-      });
-   
-
-      if (!response.ok) {
-        throw new Error('Something went wrong! catch updatePlace');
+          openingHours,
+        }),
       }
+    );
 
-      dispatch({
-        type: UPDATE_PLACE,
-        //placeId: id,
-        placeData: {
-          id: id,
-          categoryId,
-          title,
-          imageUrl,
-          location,
-          openingHours
-        }    
+    if (!response.ok) {
+      throw new Error("Something went wrong! catch updatePlace");
+    }
+
+    dispatch({
+      type: UPDATE_PLACE,
+      //placeId: id,
+      placeData: {
+        id: id,
+        categoryId,
+        title,
+        imageUrl,
+        location,
+        openingHours,
+      },
     });
-    };
   };
-  
+};
 
 // export const toggleFavorite = (id) => {
 //     return {type: TOGGLE_FAVORITE, placeId: id};
 // };
 
-export const toggleFavorite = (id) => {
-    return async dispatch => {
-      const date = new Date();
-      const response = await fetch(
-          'https://my-project-fdbf6.firebaseio.com/favorite/u1.json?',
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              id,
-              date: date.toISOString()
-            })
-          }
-        );
-    
-        if (!response.ok) {
-          throw new Error('Something went wrong! catch toggleFavorite');
-        }
-    
-        const resData = await response.json();
+// export const toggleFavorite = (id) => {
+//     return async dispatch => {
+//       const date = new Date();
+//       const response = await fetch(
+//           'https://my-project-fdbf6.firebaseio.com/favorite/u1.json?',
+//           {
+//             method: 'POST',
+//             headers: {
+//               'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({
+//               id,
+//               date: date.toISOString()
+//             })
+//           }
+//         );
 
-    dispatch({
-      type: TOGGLE_FAVORITE, 
-      placeId: id,
-      orderData: {  
-      id: resData.name
-      }
-    });
-  };
-};
+//         if (!response.ok) {
+//           throw new Error('Something went wrong! catch toggleFavorite');
+//         }
 
-export const setFilters = filterSettings => {
-    return {type: SET_FILTERS, filters: filterSettings};
-};
+//         const resData = await response.json();
+
+//     dispatch({
+//       type: TOGGLE_FAVORITE,
+//       placeId: id,
+//       orderData: {
+//       id: resData.name
+//       }
+//     });
+//   };
+// };
+
+// export const setFilters = filterSettings => {
+//     return {type: SET_FILTERS, filters: filterSettings};
+// };
--- store/reducers/places.js
+++ store/reducers/places.js
@@ -1,184 +1,177 @@
-import Place from '../../models/place';
+import Place from "../../models/place";
 
-import { PLACE } from '../../data/dummy-data';
-import { 
-    TOGGLE_FAVORITE, 
-    SET_FILTERS, 
-    DELETE_PLACE,
-    CREATE_PLACE,
-    UPDATE_PLACE,
-    SET_PLACES
-} from '../actions/places';
+import { PLACE } from "../../data/dummy-data";
+import {
+  DELETE_PLACE,
+  CREATE_PLACE,
+  UPDATE_PLACE,
+  SET_PLACES,
+} from "../actions/places";
+//import { TOGGLE_FAVORITE, SET_FILTERS } from "../actions/catalog";
 
 const initialState = {
-    //category: CATEGORIES,
-    places: PLACE,
-    filteredPlaces: PLACE,
-    favoritePlaces: [],
-    userPlaces: PLACE.filter(id => id.ownerId === 'u1')
-    //userPlaces: []
-
+  //category: CATEGORIES,
+  places: PLACE,
+  //   filteredPlaces: PLACE,
+  //   favoritePlaces: [],
+  userPlaces: PLACE.filter((id) => id.ownerId === "u1"),
+  //userPlaces: []
 };
 
 export default (state = initialState, action) => {
-   switch (action.type){
-        case SET_PLACES:
-            return {
-                //places: state.places.concat(action.places.filter(id => action.places.id in state.places.id)),
-                places: state.places.concat(action.places),
-                filteredPlaces: state.places,
-                //userPlaces: action.places.filter(id => id.ownerId === 'u1'),
-                userPlaces: action.places,
-                favoritePlaces: state.favoritePlaces
-            };
-       
-        case CREATE_PLACE:
-            const newPlace = new Place(
-                action.placeData.id,
-                action.placeData.categoryId,
-                'u1',
-                action.placeData.title,
-                action.placeData.imageUrl,
-                action.placeData.location,
-                action.placeData.openingHours,
-                false,
-                false
-            );
-            return {
-                ...state,
-                places: state.places.concat(newPlace),
-                filteredPlaces: state.places,
-                //filteredPlaces: state.places.concat(newPlace),
-                userPlaces: state.userPlaces.concat(newPlace)
-            };
-        case UPDATE_PLACE:
-            const placeIndex = state.userPlaces.findIndex(
-                prod => prod.id === action.id
-            );
+  switch (action.type) {
+    case SET_PLACES:
+      return {
+        //places: state.places.concat(action.places.filter(id => action.places.id in state.places.id)),
+        places: state.places.concat(action.places),
+        filteredPlaces: state.places,
+        //userPlaces: action.places.filter(id => id.ownerId === 'u1'),
+        userPlaces: action.places,
+        favoritePlaces: state.favoritePlaces,
+      };
 
-            const updatedPlace = new Place(
-                action.id,
-                action.placeData.categoryId,
-                'u1',
-                action.placeData.title,
-                action.placeData.imageUrl,
-                action.placeData.location,
-                action.placeData.openingHours,
-                //state.userPlaces[placeIndex].isOpenShabbath,
-                false,
-                false
+    case CREATE_PLACE:
+      const newPlace = new Place(
+        action.placeData.id,
+        action.placeData.categoryId,
+        "u1",
+        action.placeData.title,
+        action.placeData.imageUrl,
+        action.placeData.location,
+        action.placeData.openingHours,
+        false,
+        false
+      );
+      return {
+        ...state,
+        places: state.places.concat(newPlace),
+        //filteredPlaces: state.places,
+        //filteredPlaces: state.places.concat(newPlace),
+        userPlaces: state.userPlaces.concat(newPlace),
+      };
+    case UPDATE_PLACE:
+      const placeIndex = state.userPlaces.findIndex(
+        (prod) => prod.id === action.id
+      );
 
-            );
+      const updatedPlace = new Place(
+        action.id,
+        action.placeData.categoryId,
+        "u1",
+        action.placeData.title,
+        action.placeData.imageUrl,
+        action.placeData.location,
+        action.placeData.openingHours,
+        //state.userPlaces[placeIndex].isOpenShabbath,
+        false,
+        false
+      );
 
-            const updatedUserPlaces = [...state.userPlaces];
-            updatedUserPlaces[placeIndex] = updatedPlace;
+      const updatedUserPlaces = [...state.userPlaces];
+      updatedUserPlaces[placeIndex] = updatedPlace;
 
-            const availablePlaceIndex = state.places.findIndex(
-                prod => prod.id === action.id
-            );
-    
-            const updatedAvailablePlaces = [...state.places];
-            updatedAvailablePlaces[availablePlaceIndex] = updatedPlace;
+      const availablePlaceIndex = state.places.findIndex(
+        (prod) => prod.id === action.id
+      );
 
-            // const updatedFilteredPlaces = [...state.filteredPlaces];
-            // updatedFilteredPlaces[availablePlaceIndex] = updatedPlace;
-             return {
-                ...state,
-                places: updatedAvailablePlaces,
-                filteredPlaces: state.places,
-                //filteredPlaces: updatedFilteredPlaces,
-                userPlaces: updatedUserPlaces
-            };
+      const updatedAvailablePlaces = [...state.places];
+      updatedAvailablePlaces[availablePlaceIndex] = updatedPlace;
 
-        case DELETE_PLACE:
-           //console.log('DELETE');
-            return {
-                ...state,
-                userPlaces: state.userPlaces.filter(
-                place => place.id !== action.id
-                ),
-                filteredPlaces: state.filteredPlaces.filter(
-                place => place.id !== action.id
-                ),
-                places: state.places.filter(
-                place => place.id !== action.id
-                )
-            };
+      // const updatedFilteredPlaces = [...state.filteredPlaces];
+      // updatedFilteredPlaces[availablePlaceIndex] = updatedPlace;
+      return {
+        ...state,
+        places: updatedAvailablePlaces,
+        filteredPlaces: state.places,
+        //filteredPlaces: updatedFilteredPlaces,
+        userPlaces: updatedUserPlaces,
+      };
 
-        case TOGGLE_FAVORITE:
-            const existingIndex = state.favoritePlaces.findIndex(place => place.id === action.placeId);
-            if (existingIndex >= 0){
-                const updateFavPlaces = [...state.favoritePlaces];
-                updateFavPlaces.splice(existingIndex, 1);
-                return {...state, favoritePlaces: updateFavPlaces };
-            }   else {
-                    const place = state.places.find(place => place.id === action.placeId);
-                    return {...state, favoritePlaces: state.favoritePlaces.concat(place) };
-            }
-        case SET_FILTERS:
-            const appliedFilters = action.filters;
-    
-            const updateFilteredPlaces = state.places.filter(place => {
-                if (appliedFilters.openShabbath && !place.isOpenShabbath){
-                    return false;
-                }
-                if (appliedFilters.openNow && !place.isOpenNow){
-                    return false;
-                }
-                // if (appliedFilters.vegan && !place.isVegan){
-                //     return false;
-                // }
-                // if (appliedFilters.vegetarian && !place.isVegetarian){
-                //     return false;
-                // }
-                // if (appliedFilters.kosher && !place.isKosher){
-                //     return false;
-                // }
-                // if (appliedFilters.notKosher && !place.isNotKosher){
-                //     return false;
-                // }
-                // if (appliedFilters.cafe && !place.isCafe){
-                //     return false;
-                // }
-                // if (appliedFilters.restaurant && !place.isRestaurant){
-                //     return false;
-                // }
-                // if (appliedFilters.delivery && !place.isDelivery){
-                //     return false;
-                // }
-                // if (appliedFilters.takeAway && !place.isTakeAway){
-                //     return false;
-                // }
-                // if (appliedFilters.fastFood && !place.isFastFood){
-                //     return false;
-                // }
-                // if (appliedFilters.grill && !place.isGrill){
-                //     return false;
-                // }
-                // if (appliedFilters.sushi && !place.isSushi){
-                //     return false;
-                // }
-                // if (appliedFilters.italian && !place.isItalian){
-                //     return false;
-                // }
-                // if (appliedFilters.asian && !place.isAsian){
-                //     return false;
-                // }
-                // if (appliedFilters.european && !place.isEuropean){
-                //     return false;
-                // }
-                // if (appliedFilters.indian && !place.isIndian){
-                //     return false;
-                // }
-                return true;
-            });
-            
-        return {...state, filteredPlaces: updateFilteredPlaces};
-        
-        
-        // default: 
-        //     return state;
-    }
-    return state;
+    case DELETE_PLACE:
+      //console.log('DELETE');
+      return {
+        ...state,
+        userPlaces: state.userPlaces.filter((place) => place.id !== action.id),
+        filteredPlaces: state.filteredPlaces.filter(
+          (place) => place.id !== action.id
+        ),
+        places: state.places.filter((place) => place.id !== action.id),
+      };
+
+    // case TOGGLE_FAVORITE:
+    //   const existingIndex = state.favoritePlaces.findIndex(
+    //     (place) => place.id === action.placeId
+    //   );
+    //   if (existingIndex >= 0) {
+    //     const updateFavPlaces = [...state.favoritePlaces];
+    //     updateFavPlaces.splice(existingIndex, 1);
+    //     return { ...state, favoritePlaces: updateFavPlaces };
+    //   } else {
+    //     const place = state.places.find((place) => place.id === action.placeId);
+    //     return { ...state, favoritePlaces: state.favoritePlaces.concat(place) };
+    //   }
+    // case SET_FILTERS:
+    //   const appliedFilters = action.filters;
+
+    //   const updateFilteredPlaces = state.places.filter((place) => {
+    //     if (appliedFilters.openShabbath && !place.isOpenShabbath) {
+    //       return false;
+    //     }
+    //     if (appliedFilters.openNow && !place.isOpenNow) {
+    //       return false;
+    //     }
+    //     // if (appliedFilters.vegan && !place.isVegan){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.vegetarian && !place.isVegetarian){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.kosher && !place.isKosher){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.notKosher && !place.isNotKosher){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.cafe && !place.isCafe){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.restaurant && !place.isRestaurant){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.delivery && !place.isDelivery){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.takeAway && !place.isTakeAway){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.fastFood && !place.isFastFood){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.grill && !place.isGrill){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.sushi && !place.isSushi){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.italian && !place.isItalian){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.asian && !place.isAsian){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.european && !place.isEuropean){
+    //     //     return false;
+    //     // }
+    //     // if (appliedFilters.indian && !place.isIndian){
+    //     //     return false;
+    //     // }
+    //     return true;
+    //   });
+
+    //   return { ...state, filteredPlaces: updateFilteredPlaces };
+
+    // default:
+    //     return state;
+  }
+  return state;
 };
-
