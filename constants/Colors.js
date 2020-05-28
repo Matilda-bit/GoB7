f8a9d3e0d96501a3981429f363d56f28480a6e07
Matilda-bit <polinaovras@gmail.com>
2020-05-28 20:43:23 +0300

    clean code

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,14 @@
 export default {
-    primary: '#f7287b',
-    accent: '#c717fc',
-    wisteria: '#8e44ad',
-    wisteria1: '#c56cf0',
-    primaryColor: '#4a1448c',
-    accentColor: '#ff6f00',
-    c1:'#ea8685',
-    c2:'#10ac84',
-    c3:'#474787',
-    c4:'#3498db',
-    c5:'#535c68',
-    c6: '#b33939'
-}
+  primary: "#f7287b",
+  accent: "#c717fc",
+  wisteria: "#8e44ad",
+  wisteria1: "#c56cf0",
+  primaryColor: "#4a1448c",
+  accentColor: "#ff6f00",
+  c1: "#ea8685",
+  c2: "#10ac84",
+  c3: "#474787",
+  c4: "#3498db",
+  c5: "#535c68",
+  c6: "#b33939",
+};
